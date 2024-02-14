66f4ccf6922cba43a5beb799716ce15489e85d37
mdrozhzhin <mdrozhzhin@gmail.com>
2024-02-14 15:46:53 +0300

    Add files

--- homework_5.py
+++ homework_5.py
@@ -1,2 +1,18 @@
-a = 0 / 4
-print(a)
+# Определяем пустые списки.
+list_of_numbers = []
+list_of_even = []
+
+# Генерируем список
+for i in range(10):
+    list_of_numbers.append(i)
+
+# Проходимся циклом по списку и добавляем все значения, которые делятся на 2 без остатка, в лист.
+for i in list_of_numbers:
+    if i % 2 == 0:
+        list_of_even.append(i)
+        print(i)
+    else:
+        pass
+
+# Используя функцию max находим наибольшее значение элемента списка.
+print(max(list_of_even))

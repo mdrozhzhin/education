b87e14b4926dce119c6a700c844626009658302b
mdrozhzhin <mdrozhzhin@gmail.com>
2024-02-15 00:09:40 +0300

    Add files

--- lecture_6_tasks.py
+++ lecture_6_tasks.py
@@ -1,15 +1,12 @@
 import random
-# Task 1
 
-# Определяем пустые списки.
-list_of_numbers = []
+print(f'Задание №1. Вариант 11.')
+
+# Определяем списки.
+list_of_numbers = [random.randint(1, 20) for _ in range(1, 10)]
 list_of_even = []
 
-# Генерируем список
-for i in range(10):
-    list_of_numbers.append(i)
-
-# Проходимся циклом по списку и добавляем все значения, которые делятся на 2 без остатка, в лист.
+# Проходимся циклом по списку и добавляем все четные значения в лист.
 for i in list_of_numbers:
     if i % 2 == 0:
         list_of_even.append(i)
@@ -21,7 +18,7 @@ print(f'Полученный список чётных чисел: {list_of_even
 print(f'Наибольший элемент списка: {max(list_of_even)}.\n')
 
 
-# Task 2
+print(f'Задание №2. Вариант 11.')
 
 randomized_list = [random.randint(1, 20) for _ in range(1, 20)]
 print(f'Случайно сгенерированный массив целых чисел: {randomized_list}.')
